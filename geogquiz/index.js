29de8c7aeb71865560a07806086551f6ed1a72db
IL <53288945+irlfslab@users.noreply.github.com>
2024-07-04 22:42:07 -0400

    Update index.js

--- geogquiz/index.js
+++ geogquiz/index.js
@@ -8,6 +8,7 @@ form.addEventListener("submit", event => {
     
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+    //console.log(userAnswers);
     
     userAnswers.forEach((answer, idx) => {
 
